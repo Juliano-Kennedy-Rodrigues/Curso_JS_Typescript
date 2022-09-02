e4676d007fcf8444cba66f91a80aed0a387ac0c9
Juliano Rodrigues <julianokennedy11@gmail.com>
2022-09-02 11:50:48 -0300

    add arquivo js para estudar conteúdos diferentes

---
+++ operadores e exercicios.js
@@ -0,0 +1,10 @@
+/* Operadores Aritiméticos
+** potenciação ----- % resto da divisão
+Ordem: () --> ** --> * e / e % --> + e - 
++= ou *= é tipo uma var mais um valor ou vezes um valor
+*/
+
+//para traduzir string para número --> parseInt (inteiro), parseFloat (decimal)
+const num1 = 2;
+const num2 = Number('1.5');
+console.log(num1+num2);
