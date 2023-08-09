29cf3dc933d7d24708b1cb5a92726091d9edc8fb
Juliano Rodrigues <julianokennedy11@gmail.com>
2023-08-09 14:22:59 -0300

    sobre for basico

---
+++ Estruturas de repetição/For/script.js
@@ -0,0 +1,14 @@
+//basic
+/* 
+for (let i=0; i <= 10; i++) {
+    const par = i % 2 === 0 ? 'par' : 'ímpar';
+    console.log(i, par);
+}
+*/
+
+/*
+const frutas = ['Maça', 'Pera', 'Uva', 'Banana', 'Laranja'];
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i]);
+}
+*/
