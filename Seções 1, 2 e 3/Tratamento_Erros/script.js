5985bb9fc92427114ead91da0eb9089bfa545e5d
Juliano Rodrigues <julianokennedy11@gmail.com>
2023-08-15 23:03:22 -0300

    sobre try, catch e finally

--- Seções 1, 2 e 3/Tratamento_Erros/script.js
+++ Seções 1, 2 e 3/Tratamento_Erros/script.js
@@ -1,3 +1,4 @@
+/*
 //try catch throw 
 
 function soma (x, y){
@@ -15,5 +16,32 @@ try{
     //console.log(erro);
     console.log("Alguma coisa está errada");
 }
+*/
 
+//try catch finally
+
+function retornaHora (data){
+    if ( data && !(data instanceof Date)){ //se data for enviada E/MAS não for uma instãncia de date 
+        throw new TypeError ("Esperando instância de Date");
+    }  
+    
+    if (!data){ // se data NÃO for enviada
+        data = new Date();
+    }
+
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false //sem time zone (AM/PM)
+    });
+}
+
+try {
+    const hora = retornaHora(11);
+    console.log(hora);
+    
+} catch (erro){
+    //tratar erro
+    console.log("Algo deu erado")
+}finally{
+    console.log("Tenha um bom dia");
+}
 
