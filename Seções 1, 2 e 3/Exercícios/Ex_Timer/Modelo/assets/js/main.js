9fa6158f2e9248ddf55f2e240dab6e7834fc017f
Juliano Rodrigues <julianokennedy11@gmail.com>
2023-08-18 23:56:49 -0300

    exercicio criar um timer com html, js, e css simples. Feito. Melhorar CSS

--- Seções 1, 2 e 3/Exercícios/Ex_Timer/Modelo/assets/js/main.js
+++ Seções 1, 2 e 3/Exercícios/Ex_Timer/Modelo/assets/js/main.js
@@ -1,13 +1,42 @@
-function Escopo(){
-    const form = document.querySelector('form');
-
-    function naoRecarrega(evento){
-        evento.preventDefault();
-
-        alert("!");
-    }
-
-    form.addEventListener('submit', naoRecarrega);
+function getTimeFromSeconds (segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'UTC'
+    })
 }
 
-Escopo();
+const contador = document.querySelector('.contador');
+const iniciar = document.querySelector('.iniciar');
+const zerar = document.querySelector('.zerar');
+const pausar = document.querySelector('.pausar');
+let segundos = 0;
+let timer;
+
+function iniciarContagem() {
+     timer = setInterval(function() {
+        segundos++;
+        contador.innerHTML = getTimeFromSeconds(segundos);
+    }, 1000)
+
+}
+document.addEventListener('click', function (e) {
+    const el = e.target;
+
+    if(el.classList.contains('iniciar')) {
+        contador.classList.remove('pausado')
+        clearInterval(timer);
+        iniciarContagem();
+    }
+
+    if(el.classList.contains('zerar')) {
+        clearInterval(timer);
+        contador.innerHTML = "00:00:00";
+        segundos = 0;
+    }
+
+    if(el.classList.contains('pausar')) {
+        clearInterval(timer);
+        contador.classList.add('pausado')
+    }
+})
